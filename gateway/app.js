678e768320eaaf34c3dad198aeb741a23cd4b94a
Marius Purici <marius.purici@molsondev.com>
2023-11-22 17:04:12 +0200

    Replace single node redis with redis cluster

--- gateway/app.js
+++ gateway/app.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import bodyParser from 'body-parser'
 import swaggerUi from 'swagger-ui-express'
 import swaggerJsdoc from 'swagger-jsdoc'
-import redis from 'redis'
+import redis from 'ioredis'
 
 const app = express()
 const port = 8030
@@ -136,22 +136,20 @@ function circuitBreaker (serviceType, serviceUrl) {
 
 app.use(bodyParser.json())
 
-const redisHost = process.env.REDIS_HOST || 'localhost'
-console.log(`Redis Host has been resolved to "${redisHost}"`)
-
-const redisClient = redis.createClient({
-  socket: {
-    host: redisHost,
-    port: 6379
-  }
-})
+const redisClient = new redis.Cluster([
+  { host: 'redis-node-1', port: 7000 },
+  { host: 'redis-node-2', port: 7001 },
+  { host: 'redis-node-3', port: 7002 },
+  { host: 'redis-node-4', port: 7003 },
+  { host: 'redis-node-5', port: 7004 },
+  { host: 'redis-node-6', port: 7005 }
+])
 
 redisClient.on('error', (err) => { console.log('Error connecting to Redis:', err) })
 redisClient.on('connect', () => { console.log('Connected to Redis') })
 redisClient.on('ready', () => { console.log('Redis client is ready') })
 redisClient.on('reconnecting', () => { console.log('Redis client is reconnecting') })
 process.on('exit', () => { redisClient.quit() })
-await redisClient.connect()
 
 // Swagger setup
 const options = {
